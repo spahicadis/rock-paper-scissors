f9fe972e865a6619653c55438dade4a06e8a8832
spahicadis <142044354+spahicadis@users.noreply.github.com>
2024-07-10 14:04:52 +0200

    First version of algorithm

--- script.js
+++ script.js
@@ -1,13 +1,75 @@
-let startSection = document.querySelector('.start')
+//global
+let playerScore = 0;
+let computerScore = 0;
+const maxRonuds = 3;
+const choices = ['rock', 'paper', 'scissors']
+let randomChoices = choices[Math.floor(Math.random() * choices.length)];
+const computerMove = randomChoices; 
+let playerMove = null;
+
+//dom
+let startSection = document.querySelector('.start');
+let mainSection = document.querySelector('.playingScreen');
 const buttonPlay = document.querySelector('.play');
+let playerScoreStatus = document.querySelector('.playerScore')
+let computerScoreStatus = document.querySelector('.computerScore');
+let playerMove1 = document.querySelector('.actionRock');
+let playerMove2 = document.querySelector('.actionPaper');
+let playerMove3 = document.querySelector('.actionScissors');
 
-
-
-function changeScreen() {
-  startSection.classList.add('display-none')
+//displayScreen
+function changeToMainScreen() {
+  startSection.classList.add('display-none');
+  mainSection.classList.add('playingScreenActive');  
 }
+buttonPlay.addEventListener('click', changeToMainScreen);
 
-buttonPlay.addEventListener('click', changeScreen )
+
+//biding
+playerMove1.addEventListener('click', function(e) {
+ playerMove = e.target.id;
+ if(computerMove == 'rock') {
+  console.log('draft')
+ } else if(computerMove == 'paper') {
+  computerScoreStatus.innerHTML = `${++computerScore}`
+ } else if(computerMove == 'scissors') {
+  playerScoreStatus.innerHTML = `${++playerScore}`
+ }
+})
+playerMove2.addEventListener('click', function(e) {
+  playerMove = (e.target.id);
+  if(computerMove == 'rock') {
+    playerScoreStatus.innerHTML = `${++playerScore}`
+   } else if(computerMove == 'paper') {
+    console.log(`draft`)
+   } else if(computerMove == 'scissors') {
+    computerScoreStatus.innerHTML = `${++computerScore}`
+   }
+})
+playerMove3.addEventListener('click', function(e) {
+  playerMove = (e.target.id);
+  if(computerMove == 'rock') {
+    computerScoreStatus.innerHTML = `${++computerScore}`
+   } else if(computerMove == 'paper') {
+    playerScoreStatus.innerHTML = `${++playerScore}`
+   } else if(computerMove == 'scissors') {
+    console.log(`draft`)
+   }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
